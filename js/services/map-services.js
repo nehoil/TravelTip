60ed16048b7f7925b5d8ece52de45cc75ce95dc8
Nehoray Ilani <neho12@gmail.com>
2020-10-28 17:01:41 +0200

    fix services issue

--- js/services/map-services.js
+++ js/services/map-services.js
@@ -5,14 +5,15 @@ export const mapService = {
 
 
 var gLocations = [
-    {id, name, lat, lng, weather, createdAt, updatedAt}
+    // {id, name, lat, lng, weather, createdAt, updatedAt}
 ];
 
 function initService(){
-    getLatLangFromStr(str)
+    console.log('str');
+    // getLatLangFromStr()
 }
 
-function getLatLangFromStr(str){
+function getLatLangFromStr(){
     // here goes the geo-api-code  
     // Nehoray
     return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${str}&key=AIzaSyCkBmq94RUL-VNdku46pXE3nt-_Z01Damo`)
