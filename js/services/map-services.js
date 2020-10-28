d4050b6ec3a3e14a107e92c6109cb85947233c98
Nehoray Ilani <neho12@gmail.com>
2020-10-28 16:36:06 +0200

    first js changes added map

---
+++ js/services/map-services.js
@@ -0,0 +1,14 @@
+export const mapService = {
+    gLocations
+}
+
+var gLocations = [
+    {id, name, lat, lng, weather, createdAt, updatedAt}
+];
+
+
+function getLatLangFromStr(){
+    // here goes the geo-api-code
+    // Nehoray
+    return Promise.resolve({lat,lang})
+}
---
+++ js/traveltip-controller.js
@@ -0,0 +1,35 @@
+'use-strict';
+
+// import { mapService } from './services/map-services.js'
+
+var gDefaultLoc = 'Central Park, New York';
+
+window.onload = () => {
+    initMap()
+    .then(renderLoc(gDefaultLoc))
+}
+
+var gMap;
+
+
+function initMap() {
+    gMap = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+    return Promise.resolve()
+}
+
+
+function onSearch(){
+
+}
+
+function onLocClick(id){
+
+}
+
+
+function renderLoc(id){
+    
+}
