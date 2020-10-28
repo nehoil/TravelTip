e4d7aba875b44dc28a54247989363dd1a359a54b
Nehoray Ilani <neho12@gmail.com>
2020-10-28 17:09:34 +0200

    added service loc

--- js/services/map-services.js
+++ js/services/map-services.js
@@ -9,17 +9,23 @@ var gLocations = [
 ];
 
 function initService(){
-    console.log('str');
-    // getLatLangFromStr()
+    getLatLangFromStr('telaviv')
 }
 
-function getLatLangFromStr(){
+function getLatLangFromStr(str){
     // here goes the geo-api-code  
     // Nehoray
     return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${str}&key=AIzaSyCkBmq94RUL-VNdku46pXE3nt-_Z01Damo`)
     .then(res => {
-        console.log(res.data);
-        return res.data
+        // console.log(res.data.results);
+        var loc = res.data.results
+        console.log(loc.geometryc);
+        var locDetails = {
+            address: loc.formatted_address,
+            lat: loc.geometry.lat,
+            lng: loc.geometry.lng,
+        }
+        
     })
     return Promise.resolve({lat,lang})
 }
